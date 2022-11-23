e42000659034d1f2353cb33fbd876f6f325b4620
Akhmad <akhmadhurshetov@gmail.com>
2022-11-23 17:16:10 +0500

    01&02

--- count_jobs.py
+++ count_jobs.py
@@ -8,4 +8,20 @@ def count_jobs(data:list, job:str) -> int:
     Returns:
         int: The number of users with the given job
     """
+    
     pass
+
+    x=0
+    for i in data:
+        if i['job']==job:
+            x+=1
+    return x
+
+
+print(count_jobs([
+    {'name': 'John', 'job': 'Developer'},
+    {'name': 'Mary', 'job': 'Developer'},
+    {'name': 'Akhmad', 'job': 'Student'},
+    {'name': 'Ulugbek', 'job': 'Developer'}], 
+    "Developer"
+    ))
--- count_users_with_age.py
+++ count_users_with_age.py
@@ -9,3 +9,14 @@ def count_users_with_age(data:list, age:int) -> int:
         int: The number of users with the given age
     """
     pass
+
+    count=0
+    for i in data:
+        if i['age']==age:
+            count+=1
+    return count
+
+print(count_users_with_age(
+    [{'name': 'John', 'age': 27},
+    {'name':'Mary', 'age': 42}],42)
+)
